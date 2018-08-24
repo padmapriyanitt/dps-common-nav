2ccdea73b8cf6faccc917f919e22224f8f170785
padmapriyanitt <padmapriyanitt@gmail.com>
2018-08-24 12:55:53 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const Index = () => {
+//   return <div>Hello React!</div>;
+// };
+
+// ReactDOM.render(<Index />, document.getElementById("index"));
+
+import { h } from "preact";
+import habitat from "preact-habitat";
+
+import Widget from "./components/nav-container";
+
+export class DPSNavigation {
+  
+ static init(options) {
+   let _habitat = habitat(Widget);
+   _habitat.render({
+     selector: `[id=${options.id}]`,
+     clean: true
+   });
+  }
+}
+
+let _habitat = habitat(Widget);
+_habitat.render({
+  selector: `#index`,
+  clean: true
+});
+//DPSNavigation.init({id:"index"})
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },{
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      },{
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin,new CopyWebpackPlugin([{ from: 'src/assets/images', to: 'images' }])]
+};

83f45cde24e01c5fa85026be3b712fc223b7801c
padmapriyanitt <padmapriyanitt@gmail.com>
2018-09-07 14:30:13 +0530

    added url prefix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import habitat from "preact-habitat";
 import DPSNavWidget from "./components/menu";
 
 export function init(options) {
-  render(<DPSNavWidget collapseOnInit={options.collapseOnInit} standalone={options.standalone} 
-    onCollapse={options.onCollapse} onExpand={options.onExpand} navigate={options.navigate} />, document.getElementById(options.id));
+  render(<DPSNavWidget collapseOnInit={options.collapseOnInit} standalone={options.standalone}
+    urlPrefix={options.urlPrefix}
+    onCollapse={options.onCollapse} onExpand={options.onExpand} navigate={options.navigate} />, 
+    document.getElementById(options.id));
 }

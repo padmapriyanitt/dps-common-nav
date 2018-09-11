fd74574cedc5f54feaeae9d6b4b0bb532ce1acb9
padmapriyanitt <padmapriyanitt@gmail.com>
2018-09-11 11:10:45 +0530

    sub menu fix

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -57,8 +57,6 @@ export default class App extends Component {
         this.markMenusToOpen(subMenuItem, subMenuItem.subMenu);
         if(window.location.href.endsWith(subMenuItem.link)){
           menuItem.subMenuOpen = true;
-        }else{
-          menuItem.subMenuOpen = false;
         }
       });
     }        
